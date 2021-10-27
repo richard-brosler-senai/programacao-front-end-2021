0b42c0beb7a8c08988f62165caa96d2e96eee38c
Richard Brosler <richard.brosler@sp.senai.br>
2021-10-27 16:50:43 -0300

    Ajustes para a aula presencial 04

---
+++ uc9/sa2/presencial/atividade-04/calculadora-temperatura/javascript/calculadora.js
@@ -0,0 +1,6 @@
+$("#limpar").click(()=>{
+  $("#tempOrigem").val("")
+  $("#tempDestino").val("0.00")
+  $("input[name=tipotempOrigem][value='1']").prop('checked',true)
+  $("input[name=tipotempDestino][value='1']").prop('checked',true)
+})
